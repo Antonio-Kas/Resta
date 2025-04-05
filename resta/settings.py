40b60cf61563f0b131cfc0688092d265812a8b6f
Ihor Kolodyuk <ihor.kolodyuk@virtuozzo.com>
2025-04-05 22:59:33 +0300

    upd

--- resta/settings.py
+++ resta/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-epou&f1uuhiqze&^oh*di!12374i1mfw4$93r4l7$is-f^wf)d4m7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -74,10 +77,15 @@ WSGI_APPLICATION = 'resta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',            
+        'HOST': os.getenv('DB_HOST', 'localhost'),       
+        'USER': os.getenv('DB_USER', 'root'),            
+        'PASSWORD': os.getenv('DB_PASS', ''),            
+        'NAME': os.getenv('DB_NAME', 'my_database'),    
+        'PORT': os.getenv('DB_PORT', '3306'),           
     }
 }
 
